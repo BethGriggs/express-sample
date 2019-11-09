066e807067837d03c8d86db62d4ecebe704afcb9
Beth Griggs <Bethany.Griggs@uk.ibm.com>
2019-11-09 22:54:02 +0000

    add health checking

--- app.js
+++ app.js
@@ -13,6 +13,10 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+// health check setup
+var health = require('@cloudnative/health-connect');
+var healthcheck = new health.HealthChecker();
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -21,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/health', health.LivenessEndpoint(healthcheck));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
